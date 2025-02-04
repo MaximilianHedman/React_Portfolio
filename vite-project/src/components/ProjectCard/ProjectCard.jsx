dd15ff3b639480df66b3824ac28eac6cdf2c4f08
MaximilianHedman <maximilian.hedman@chasacademy.se>
2025-02-04 20:29:36 +0100

    created data for projects that i import and map out in ProjectCard

--- vite-project/src/components/ProjectCard/ProjectCard.jsx
+++ vite-project/src/components/ProjectCard/ProjectCard.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { projectData } from '../../data/projectData';
 import './ProjectCard.css';
 
 const ProjectCard = () => {
     return (
-        <a className="card-link" href="https://www.figma.com/design/Ql9U2CxELqaTBtVrBhOkn4/V%C3%84NNER?node-id=793-5315&t=lXZwuEP3nbLxBmQV-1" target="_blank">
-            <div className="card">
-                <div className="card-img">
-                    <img className="card-img" src="../../assets/figma_Vanner.svg" alt="Figma Vanner" />
-                </div>
-                <div className="card-text">
-                    <h4>VÄNNER App</h4>
-                    <p>An app to find new friends & activities</p>
-                </div>
-            </div>
-        </a>
-    )
-}
+        <div className="project-cards-container">
+            {projectData.map((project) => (
+                <a className="card-link" href={project.link} key={project.id} target="_blank" rel="noopener noreferrer">
+                    <div className="card">
+                        <div className="card-img">
+                            <img src={project.image} alt={project.title} />
+                        </div>
+                        <div className="card-text">
+                            <h4>{project.title}</h4>
+                            <p>{project.description}</p>
+                        </div>
+                    </div>
+                </a>
+            ))}
+        </div>
+    );
+};
 
-export default ProjectCard
+export default ProjectCard;
---
+++ vite-project/src/data/projectData.js
@@ -0,0 +1,27 @@
+import figmaVanner from '../assets/figma_Vanner.svg';
+import figmaFoodApp from '../assets/figma_Food_app.svg';
+import figmaDevBnB from '../assets/figma_DevBnB.svg';
+
+export const projectData = [
+    {
+        id: 1,
+        title: 'VÄNNER App',
+        description: 'An app to find new friends & activities',
+        image: figmaVanner,
+        link: 'https://www.figma.com/design/Ql9U2CxELqaTBtVrBhOkn4/V%C3%84NNER?node-id=793-5315&t=lXZwuEP3nbLxBmQV-1',
+    },
+    {
+        id: 2,
+        title: 'Food App',
+        description: 'An app to order food & ingredients',
+        image: figmaFoodApp,
+        link: 'https://www.figma.com/design/lPkoTKAivjPEvwXcDFtlTn/Food-app?t=tUORXdu73FpuWzSl-1',
+    },
+    {
+        id: 3,
+        title: 'DevBnB Website',
+        description: 'An airbnb website for coders',
+        image: figmaDevBnB,
+        link: 'https://www.figma.com/design/sQMMYLChY0uwOk49JafqaZ/Frontend-Project-DevBnB?node-id=0-1&t=tUORXdu73FpuWzSl-1',
+    },
+];
