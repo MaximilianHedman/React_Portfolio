79e4d4caa639ee71105ee127320cda62f94e7778
MaximilianHedman <maximilian.hedman@chasacademy.se>
2025-02-07 18:41:05 +0100

    somthing

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Footer from './components/Footer/Footer';
+import BackToTopBtn from "./components/BackToTopBtn/BackToTopBtn";
 
 const App = () => {
     return (
